d7ff44db89336382a6f78e1815d1a06dc459d314
nakamura-kyota <>
2020-01-17 20:42:10 +0900

    add http req

--- sample/guessing_game/src/main.rs
+++ sample/guessing_game/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
+    // println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
 
     loop {
         println!("Please input your guess.");   // ほら、予想を入力してね
@@ -19,15 +19,20 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");     // 行の読み込みに失敗しました
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);     // 次のように予想しました: {}
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
